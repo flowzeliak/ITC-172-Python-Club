1f0c9012329b7daaf67419488f4fc3256dc72743
flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2019-01-24 03:25:54 -0800

    Assignment 4 - Fixed

--- pythonclub/club/models.py
+++ pythonclub/club/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField(blank=True, null=True)
+    meetinglocation=models.CharField(max_length=255)
+    agenda=models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+
+class MeetingMinutes(models.Model):
+    meetingID=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance=models.ManyToManyField(User)
+    minutestext=models.TextField()
+
+    def __str__(self):
+        return self.meetingID
+
+    class Meta:
+        db_table='meetingminutes'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255, null=True, blank=True)
+    resourceurl=models.URLField(null=True, blank=True)
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.TextField()
+
+    def __str__(self):
+        return self.resourcename
+
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    eventdate=models.DateField()
+    eventtime=models.TimeField(blank=True, null=True)
+    eventdescription=models.TextField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.eventtitle
+
+    class Meta:
+        db_table='event'
+
+    
