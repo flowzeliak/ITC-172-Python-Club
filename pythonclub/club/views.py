3c3ee06815249a7dfe7825028459d6fdbc6af09e
flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2019-02-02 12:55:47 -0800

    Assignment 5 Added

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def techtypes (request):
+    type_list=Resource.objects.all()
+    return render (request, 'club/types.html', {'type_list': type_list})
+
