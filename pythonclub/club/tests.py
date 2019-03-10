4180351a95c879b292863a5dd70aa76de8e9970d
flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2019-03-10 16:43:58 -0700

    Assignment 10

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import Meeting, MeetingMinutes, Resource, Event 
+from .forms import MeetingForm, ResourceForm
+from datetime import datetime
 from django.urls import reverse
 
 # Create your tests here.
@@ -44,3 +46,29 @@ class TestIndex(TestCase):
     def test_view_uses_correct_template(self):
         response=self.client.get(reverse('index'))
         self.assertTemplateUsed(response, 'club/index.html')
+
+class TestGetMeeting(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/club/getmeeting/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('getmeeting'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('getmeeting'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club/meeting.html')
+
+class New_Meeting_Form_Test(TestCase):
+
+    def test_MeetingForm_is_valid(self):
+        form = MeetingForm(data={'meetingtitle':"Business Meeting", 'meetingdate':"2019-02-22", 'meetingtime':"09:30:00", 'meetinglocation':"The Business Office", 'agenda':"Talkin' Business"})
+        self.assertTrue(form.is_valid())
+
+
+    def test_MeetingForm_invalid(self):
+        form = MeetingForm(data={'productname': "Surface", 'techtype': "laptop", 'user': "steve", 'entrydate': "2018-12-17", 'productURL':"http:microsoft.com", 'productdescription':"lightweight laptop"})
+        self.assertFalse(form.is_valid())
